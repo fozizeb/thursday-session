739fc1df953c470fd48401efbd10e9908567f778
fouzia <fouzia@LAPTOP-7TR0M7AC.myfiosgateway.com>
2021-12-09 17:30:40 -0500

    course

---
+++ automationpro/src/main/java/protrainag/automationpro/App.java
@@ -0,0 +1,13 @@
+package protrainag.automationpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
